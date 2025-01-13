046ef755a1f98453875c6ee684f041ae5208c09a
Admin <admin@Admins-MacBook-Pro.local>
2025-01-13 21:25:51 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Logo from "./components/Logo";
+import ThemeSwitcher from "./components/ThemeSwitcher";
+
+function App() {
+  return (
+    <>
+      <Logo />
+      <ThemeSwitcher />
+    </>
+  );
+}
+
+export default App;
