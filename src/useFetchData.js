084e558f0cfb458ad2e1dbb476b5641117185794
Admin <admin@Admins-MacBook-Pro.local>
2025-01-22 15:48:26 +0100

    add useCallback and dependencies of useEffect

--- src/useFetchData.js
+++ src/useFetchData.js
@@ -1,18 +1,11 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 export const useFetchData = (country) => {
   const [result, setResult] = useState([]);
   const [filteredCountries, setFilteredCountries] = useState([]);
   const [isError, setIsError] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-  useEffect(() => {
-    if (country) {
-      fetchDataFormAPI();
-    } else {
-      fetchDataFormLocalStorage();
-    }
-  }, []);
-  const fetchDataFormAPI = () => {
+
+  const fetchDataFormAPI = useCallback(() => {
     let url = "https://restcountries.com/v3.1/all";
     setIsLoading(true);
     if (country) {
@@ -31,9 +24,9 @@ export const useFetchData = (country) => {
       })
       .catch(() => setIsError(true))
       .finally(() => setIsLoading(false));
-  };
+  }, [country]);
 
-  const fetchDataFormLocalStorage = () => {
+  const fetchDataFormLocalStorage = useCallback(() => {
     const data = JSON.parse(localStorage.getItem("countries"));
     if (data) {
       setResult(data);
@@ -41,7 +34,15 @@ export const useFetchData = (country) => {
     } else {
       fetchDataFormAPI();
     }
-  };
+  }, [fetchDataFormAPI]);
+  useEffect(() => {
+    if (country) {
+      fetchDataFormAPI();
+    } else {
+      fetchDataFormLocalStorage();
+    }
+  }, [country, fetchDataFormAPI, fetchDataFormLocalStorage]);
+
   return {
     result,
     filteredCountries,
