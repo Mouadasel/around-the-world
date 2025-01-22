3a5a35a17bf256d14448481cf4cacdc4d4c58cad
Admin <admin@Admins-MacBook-Pro.local>
2025-01-22 17:33:32 +0100

    fix SearchRegion

--- src/components/RegionMenu.jsx
+++ src/components/RegionMenu.jsx
@@ -2,31 +2,34 @@
 import Select from "react-select";
 
 const options = [
-  { value: "all regions", label: "All Regions" },
+  { value: "all regions", label: "All regions" },
   { value: "africa", label: "Africa" },
-  { value: "america", label: "America" },
   { value: "asia", label: "Asia" },
   { value: "europe", label: "Europe" },
   { value: "oceania", label: "Oceania" },
 ];
+
 const RegionMenu = ({ countriesList, filterCountriesList }) => {
   const handleRegionChange = (e) => {
     const region = e.label;
-    region === "All Regions"
-      ? countriesList
-      : countriesList.filter((country) => country.region === region);
-    filterCountriesList(filterCountriesList);
+
+    const filteredCountries =
+      region === "All regions"
+        ? countriesList
+        : countriesList.filter((country) => country.region === region);
+
+    filterCountriesList(filteredCountries);
   };
   return (
     <Select
       defaultValue={options[0]}
-      options={options}
       onChange={handleRegionChange}
-      className={{
+      options={options}
+      classNames={{
         input: () => "dark:!text-gray-100",
         singleValue: () => "dark:text-gray-100",
         control: () =>
-          "flex h-12 items-center justify-between gap-12 rounded-full !border-none pl-4 pr-2 shadow",
+          "flex h-12 items-center justify-between gap-12 rounded-md !border-none pl-4 pr-2 shadow dark:bg-gray-800 dark:text-gray-100 md:h-14",
         indicatorSeparator: () => "hidden",
         option: () => "hover:!text-gray-800",
         menu: () => "bg-gray-100 dark:bg-gray-800 dark:text-gray-100",
